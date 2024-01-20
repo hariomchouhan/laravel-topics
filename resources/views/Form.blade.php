1051e31c4df41b7f9c4f3a796e7324e16900a735
Hariom Chouhan <hariomchouhan430@gmail.com>
2024-01-20 14:39:31 +0530

    Add form validation

--- resources/views/Form.blade.php
+++ resources/views/Form.blade.php
@@ -13,9 +13,31 @@
             <h1 class="text-center text-white p-2 rounded bg-primary ">Registration</h1>
         <form action="{{url('/')}}/register" method="post">
         @csrf
-            <input type="text" name="name" placeholder="Enter your Name" class="form-control mt-5">
-            <input type="email" name="email" placeholder="Enter your Email" class="form-control mt-3">
-            <input type="password" name="password" placeholder="Enter your Password" class="form-control mt-3">
+            <input type="text" name="name" placeholder="Enter your Name" class="form-control mt-5" value="{{old('name')}}">
+            <span class="text-danger">
+                @error('name')
+                    {{$message}}
+                @enderror
+            </span>
+            <input type="email" name="email" placeholder="Enter your Email" class="form-control mt-3" value="{{old('email')}}">
+            <span class="text-danger">
+                @error('email')
+                    {{$message}}
+                @enderror
+            </span>
+            <input type="password" name="password" placeholder="Enter your Password" class="form-control mt-3" value="{{old('password')}}">
+            <span class="text-danger">
+                @error('password')
+                    {{$message}}
+                @enderror
+            </span>
+            <input type="password" name="password_confirmation" placeholder="Enter your Password" class="form-control mt-3" value="{{old('password_confirmation')}}">
+            <span class="text-danger">
+                @error('password_confirmation')
+                    {{$message}}
+                @enderror
+            </span>
+            <br>
             <button class="btn btn-primary mt-4">Submit</button>
         </form>
         </div>
