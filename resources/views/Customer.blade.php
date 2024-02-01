0ac5915f56cc6303f75ff119b771389342d2d864
Hariom Chouhan <hariomchouhan430@gmail.com>
2024-02-01 14:36:41 +0530

    Laravel Collective HTML form Package and Installation and also few issues like select tag is not take a value to given in the database and date tag is not empty in case of create a new user it take by default value of 01/01/1970 like this

--- resources/views/Customer.blade.php
+++ resources/views/Customer.blade.php
@@ -6,63 +6,100 @@
     <div class="col-6 mx-auto shadow p-5 mt-5 rounded">
         <h1 class="text-center text-white p-2 rounded bg-primary ">{{$title}}</h1>
 
-        <form action="{{$url}}" method="post">
-            @csrf
+        <!-- <form action="{{$url}}" method="post"> -->
+        {!! Form::open([
+        'url' => $url,
+        'method' => 'post',
+        'id' => 'contact',
+        'role' => 'form',
+        'class' => 'bv-form',
+        'enctype' => 'multipart/form-data'
+        ]) !!}
+        <!-- @csrf -->
 
-            <div class="form-group row">
-                <input type="text" name="name" placeholder="Enter your Name" label="Name" value="{{$customer->name}}" class="col mx-2" />
-                <input type="email" name="email" placeholder="Enter your email" label="Email"
-                    value="{{$customer->email}}" class="col mx-2" />
-            </div>
+        <div class="form-group row">
+            <!-- <input type="text" name="name" placeholder="Enter your Name" label="Name" value="{{$customer->name}}" class="col mx-2" /> -->
+            {!! Form::text('name', $customer->name, [
+            'id' => "name", 'required' => "", 'placeholder' => "Name", 'class' => "col mx-2"
+            ]) !!}
+            <input type="email" name="email" placeholder="Enter your email" label="Email" value="{{$customer->email}}"
+                class="col mx-2" />
+        </div>
 
-            <div class="form-group row">
-                <input type="text" name="state" placeholder="Enter your state" label="State"
-                    value="{{$customer->state}}" class="col mx-2"  />
-                <input type="text" name="country" placeholder="Enter your Country" label="Country"
-                    value="{{$customer->country}}" class="col mx-2"  />
-            </div>
+        <div class="form-group row">
+            <input type="text" name="state" placeholder="Enter your state" label="State" value="{{$customer->state}}"
+                class="col mx-2" />
+            {!! Form::select('country', [
+            "1" => "Afghanistan",
+            "2" => "Albania",
+            "3" => "Algeria",
+            "4" => "Andorra",
+            "5" => "Angola",
+            "6" => "Anguilla",
+            "7" =>"Antigua & Barbuda",
+            "8" => "Argentina",
+            "9" => "Armenia",
+            "10" => "Aruba",
+            "11" => "Australia",
+            "12" => "Austria",
+            "13" => "Azerbaijan",
+            "14" => "Bahamas",
+            "15" => "Bahrain",
+            "16" => "Bangladesh",
+            "17" => "Barbados",
+            "18" => "Belarus",
+            "19" => "Belgium",
+            "20" => "Belize",
+            ],
+            '5', [
+            'id' => "name", 'required' => "", 'placeholder' => "Name", 'class' => "col mx-2"
+            ]) !!}
+        </div>
 
-            <div class="form-group row">
-                <input type="text" name="address" placeholder="Enter your Address" label="Address"
-                    value="{{$customer->address}}" class="col mx-2"  />
-                <input type="password" name="password" placeholder="Enter your Password" label="Password" class="col mx-2"  />
-            </div>
+        <div class="form-group row">
+            <input type="text" name="address" placeholder="Enter your Address" label="Address"
+                value="{{$customer->address}}" class="col mx-2" />
+            <input type="password" name="password" placeholder="Enter your Password" label="Password"
+                class="col mx-2" />
+        </div>
 
-            <div class="form-group">
-                <label>Gender</label>
-                <label for="male" class="mx-2">
-                <input type="radio" name="gender" id="male" value="M"  {{$customer->gender == "M" ? "checked" : ""}}>
+        <div class="form-group">
+            <label>Gender</label>
+            <label for="male" class="mx-2">
+                <input type="radio" name="gender" id="male" value="M" {{$customer->gender == "M" ? "checked" : ""}}>
                 Male</label>
-                <label for="female" class="mx-2">
-                <input type="radio" name="gender" id="female" value="F"  {{$customer->gender == "F" ? "checked" : ""}}>
+            <label for="female" class="mx-2">
+                <input type="radio" name="gender" id="female" value="F" {{$customer->gender == "F" ? "checked" : ""}}>
                 Female</label>
-                <label for="other" class="mx-2">
+            <label for="other" class="mx-2">
                 <input type="radio" name="gender" id="other" value="O" {{$customer->gender == "O" ? "checked" : ""}}>
                 Other</label>
-                
-            </div>
 
-            <div class="form-group row">
-                <input type="date" name="dob" placeholder="Enter Date" label="Date" value="{{$customer->dob}}" class="col mx-2"  />
-                <input type="number" name="points" placeholder="Enter your points" label="Points"
-                    value="{{$customer->points}}" class="col mx-2"  />
-            </div>
+        </div>
 
-            <div class="form-group">
-                <label>Status</label>
-                <label for="active" class="mx-2" >
-                    <input type="radio" name="status" value="1" id="active" {{$customer->status == "1" ? "checked" :
-                    ""}}/>
-                    Active</label>
+        <div class="form-group row">
+            <input type="date" name="dob" placeholder="Enter Date" label="Date" value="{{$customer->dob}}"
+                class="col mx-2" />
+            <input type="number" name="points" placeholder="Enter your points" label="Points"
+                value="{{$customer->points}}" class="col mx-2" />
+        </div>
 
-                <label for="deactive"  class="mx-2"  >
-                    <input type="radio" name="status" value="0" id="deactive" {{$customer->status == "0" ? "checked" :
-                    ""}}/>
-                    Deactive</label>
-            </div>
+        <div class="form-group">
+            <label>Status</label>
+            <label for="active" class="mx-2">
+                <input type="radio" name="status" value="1" id="active" {{$customer->status == "1" ? "checked" :
+                ""}}/>
+                Active</label>
 
-            <button class="btn btn-primary">Submit</button>
-        </form>
+            <label for="deactive" class="mx-2">
+                <input type="radio" name="status" value="0" id="deactive" {{$customer->status == "0" ? "checked" :
+                ""}}/>
+                Deactive</label>
+        </div>
+
+        <button class="btn btn-primary">Submit</button>
+        <!-- </form> -->
+        {!! Form::close() !!}
     </div>
 </div>
 @endsection
